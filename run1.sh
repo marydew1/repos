8842225aa0f82936d40384d41d55201c8a3fac17
marydew1 <marydew@inbox.ru>
2022-10-31 19:47:06 +0300

    run1 updated

--- run1.sh
+++ run1.sh
@@ -1,5 +1,9 @@
 !/bin/bash
-
-while read file; do
-	wget ${file}
-done < $0
+file_name=$0
+workers=$1
+column_links=$2
+output_folder=$3
+threads=10
+mkdir -p newdir
+cat (file_name) | $awk -v columns= (column_links) -F ";" '{print column_links}' | xargs -n(threads) -P(workers) wget;
+cd (newdir) | cat (output_folder) | awk -v columns=(column_links) -F ";" '{print $columns}' | xargs -n(workers) -P(threads) wget;
