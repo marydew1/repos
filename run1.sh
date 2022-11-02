20d72ff963f87ddc490357fbe7899639da7b2994
marydew1 <marydew@inbox.ru>
2022-11-02 13:46:00 +0300

    file changed

--- run1.sh
+++ run1.sh
@@ -1,9 +1,9 @@
-!/bin/bash
-file_name=$0
+#!/bin/bash
+file_name="C:\Users\Mary\Desktop\labelled_newscatcher_dataset.csv"
+echo $file_name
 workers=$1
 column_links=$2
 output_folder=$3
-threads=10
 mkdir -p newdir
-cat (file_name) | $awk -v columns= (column_links) -F ";" '{print column_links}' | xargs -n(threads) -P(workers) wget;
-cd (newdir) | cat (output_folder) | awk -v columns=(column_links) -F ";" '{print $columns}' | xargs -n(workers) -P(threads) wget;
+cat $file_name | awk -v columns=2 -F ";" '{print $columns}' | xargs -n3 -P10 wget;
+cd $newdir | cat $output_folder | awk -v columns=$column_links -F ";" '{print $columns}' | xargs -n$workers -P$10 wget;
